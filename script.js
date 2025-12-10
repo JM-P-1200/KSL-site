ad702e6b456f17d48b21833dae04731b3918b72b
Japh Palmares <japhmiguel@gmail.com>
2025-12-11 00:49:53 +0800

    updated version

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ fetch('data/projects.json')
 .then(function(r){ if(!r.ok) throw new Error('Failed to load projects.json'); return r.json(); })
 .then(function(json){
 container.innerHTML = json.projects.map(function(p){
-return '<div class="card glass-min"><h3>${p.title}</h3><p>${p.description}</p><a href="${p.link}" class="btn-secondary">View Code</a></div>';
+return '<div class="card glass-min">
+<h3>'+p.title+'</h3>
+<p>'+p.description+'</p>
+<a href="'+p.link+'" class="btn-secondary">View Code</a>
+</div>';
 }).join('');
 })
 .catch(function(err){ container.innerHTML = '<p class="error">Unable to load projects: '+err.message+'</p>'});
